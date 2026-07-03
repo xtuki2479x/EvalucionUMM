f104c2f6d475ebbb4ce21b147af5ff819767a2cd
Tuki <d.tapia80@alumnos.santotomas.cl>
2026-07-02 20:51:09 -0400

    Base de datos

---
+++ database.sql
@@ -0,0 +1,10 @@
+CREATE DATABASE evaluacionUMM;
+
+USE evaluacionUMM;
+
+CREATE TABLE usuarios(
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    nombre VARCHAR(100) NOT NULL,
+    correo VARCHAR(100) UNIQUE NOT NULL,
+    password VARCHAR(255) NOT NULL
+);
